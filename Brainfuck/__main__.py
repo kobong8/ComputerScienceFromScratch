6727a14fbc64375a81543cd1fbb57f160537f2ca
kobong8 <fblsj90@gmail.com>
2026-02-02 21:21:16 +0900

    run code

--- Brainfuck/__main__.py
+++ Brainfuck/__main__.py
@@ -19,7 +19,8 @@ from Brainfuck.brainfuck import Brainfuck
 if __name__ == "__main__":
     # Parse the file argument
     file_parser = ArgumentParser("Brainfuck")
-    file_parser.add_argument("brainfuck_file",
-                             help="A file containing Brainfuck source code.")
+    file_parser.add_argument(
+        "brainfuck_file", help="A file containing Brainfuck source code."
+    )
     arguments = file_parser.parse_args()
     Brainfuck(arguments.brainfuck_file).execute()
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - 모든 자료의 내용은 "[Computer Science From Scratch Building Interpreters, Art, Emulators and ML in Python by David Kopec (2025)](https://nostarch.com/computer-science-from-scratch)" 을 공부하며 작성한 코드 입니다.
 
 - 저자의 GitHub 링크: [davecom/ComputerScienceFromScratch](https://github.com/davecom/ComputerScienceFromScratch)
+
+```powershell
+ComputerScienceFromScratch> python -m Brainfuck Brainfuck\Examples\fibonacci.bf
+```
